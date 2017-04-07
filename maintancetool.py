37bde8057621324ae566c632ba17b0c8d126258e
Neeky <1721900707@qq.com>
2017-04-07 14:06:10 +0800

    add maintance tool

---
+++ maintancetool.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+
+from __future__ import print_function
+from mysql import connector
+import logging,argparse,sys
+
+
+if __name__=="__main__":
+    print("hello world")
+
