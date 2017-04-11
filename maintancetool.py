2ce7387d07f6e873c2d86c836968c5aea502c2c4
Neeky <1721900707@qq.com>
2017-04-11 10:55:06 +0800

    add maintancetool.purgbinarylogs

--- maintancetool.py
+++ maintancetool.py
@@ -3,7 +3,7 @@
 
 from __future__ import print_function
 from mysql import connector
-import argparse,sys
+import argparse,sys,os
 
 """
 作者:蒋乐兴
@@ -13,13 +13,15 @@ import argparse,sys
 #create user monitoruser@'127.0.0.1' identified by '123456';
 #grant replication client on *.* to monitoruser@'127.0.0.1';
 #grant super on *.* to monitoruser@'127.0.0.1';
-
+#create user monitoruser@'%' identified by '123456';
+#grant replication client on *.* to monitoruser@'%';
+#grant super on *.* to monitoruser@'%';
 
 class BaseMysqlMaintance(object):
     """
     作为所有运维操作的基类
     """
-    def __init__(self,user='monitoruser',password='123456',host='127.0.0.1',port=3306):
+    def __init__(self,user='monitoruser',password='123456',host='172.16.192.100',port=3306):
         """初始化属性与到数据库端的连接"""
         self.user=user
         self.password=password
@@ -30,7 +32,8 @@ class BaseMysqlMaintance(object):
         try:
             config={'user':self.user,'password':self.password,'host':self.host,'port':self.port}
             self.cnx=connector.connect(**config)
-            self.cursor=self.cnx.cursor(prepared=True)
+            #self.cursor=self.cnx.cursor(prepared=True)
+            self.cursor=self.cnx.cursor(raw=True)
         except connector.Error as err:
             """如果连接失败就赋空值"""
             self.cnx=None
@@ -57,6 +60,23 @@ class BaseMysqlMaintance(object):
         raise NotImplemented("请在子类中实现具体操作...")
 
 
+class PurgeBinaryLogs(BaseMysqlMaintance):
+    """清理二进行日志文件"""
+    #在清理二进制日志文件时留remainFileCount个文件不清理
+    remainFileCount=2
+    def action(self):
+        #拿到当前的二进制文件信息
+        self.cursor.execute("show master status")
+        info=self.cursor.fetchone()[0].decode('utf-8')
+        (currentFileName,currentFileIndex)=info.split('.')
+        #根据当前的二进制文件信息 & 要保留的文件数量 确定出要执行的purge binary logs to {} 语句
+        fileIndex=str(int(currentFileIndex)-self.remainFileCount)
+        stmt="purge binary logs to '{0}.{1:0>6}' ;".format(currentFileName,fileIndex)
+        #由于当前的mysql-connector不支持执行purge binary logs to 语句 所以只能去调用bash接口啦
+        cmd="""mysql -h{0} -P{1} -u{2} -p{3} -e"{4}" &""".format(self.host,self.port,self.user,self.password,stmt)
+        os.popen(cmd)
+
+
 
 
 
@@ -70,7 +90,7 @@ if __name__=="__main__":
     parser.add_argument('--port',default=3306,type=int,help='mysql port')
     parser.add_argument('--version',default='1.0.0',help='1.0.0')
     args=parser.parse_args()
-    mt=BaseMysqlMaintance()
+    mt=PurgeBinaryLogs()
     mt.action()
 
 """
