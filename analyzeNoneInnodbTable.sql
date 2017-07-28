2ff41010e8aad579c18ff6b1d2821daf084fc66f
Neeky <1721900707@qq.com>
2017-07-28 21:47:28 +0800

    17.07.28.x 按功能组织代码

--- analyzeNoneInnodbTable.sql
+++
@@ -1,4 +0,0 @@
-# 执行当前实例中业务库中的非innodbb表
-
-select concat(table_schema,'.',table_name) as noneInnodbTable from information_schema.tables
-    where table_schema not in ('information_schema','performance_schema','mysql','sys') and engine != 'InnoDB';
