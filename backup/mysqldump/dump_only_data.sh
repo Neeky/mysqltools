e868c83a9396dfc061fe890b5ecf34d30224e04b
Neeky <1721900707@qq.com>
2017-08-26 22:43:22 +0800

    17.08.26.03 备份 数据迁移

--- backup/mysqldump/dump_only_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mysqldump --skip-extended-insert --no-create-db --no-create-info \
-          --skip-lock-tables --skip-add-locks --set-gtid-purged=OFF --single-transaction \
-          --databases tempdb > /tmp/tempdb.sql
