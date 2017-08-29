6cea77d0d608b5e63d5d06cc45628105096680e0
Neeky <1721900707@qq.com>
2017-08-29 16:12:39 +0800

    17.08.29.02 备份 调整mysqlbackup备份脚本

--- backup/meb/mysqlbackup_exsample.sh
+++ backup/meb/mysqlbackup_exsample.sh
@@ -2,8 +2,8 @@
 
 # 0001对整个实例进行备份
 mysqlbackup --defaults-file=/etc/my.cnf --backup-dir=/tmp/ --with-timestamp \
-  --host=127.0.0.1 --port=3306 --user=backupuser --password=1234567890 \
-  --compress --skip-binlog --skip-relaylog --no-locking \
+  --host=127.0.0.1 --port=3306 --user=mebuser --password='Pass@352' \
+  --compress --skip-binlog --skip-relaylog \
   --read-threads=4 --process-threads=8 --write-threads=4 --limit-memory=1024 \
   --backup-image=/tmp/$(date +'%F_%H-%M-%S').mbi \
   backup-to-image
