07667ecf388ac37ba77f2a6a4c36f1d8b8e07754
Neeky <1721900707@qq.com>
2018-04-25 16:11:52 +0800

    2.18.04.25.0 兼容到mysql-8.0.x 版本

--- mysqltoolsclient/mxb.py
+++ mysqltoolsclient/mxb.py
@@ -18,20 +18,24 @@
 """
 
 import os,argparse
+from datetime import datetime
 
 class BackupTool(object):
     host='127.0.0.1'
     port=3306
     user='backuper'
     password='mtls0352'
+    backup_script=None
+    backup_save_dir='/database/backup/mysql/'
 
-    def __init__(self,host='127.0.0.1',port=3306,user='backuper',password='mtls0352'):
+    def __init__(self,host='127.0.0.1',port=3306,user='backuper',password='mtls0352',backup_save_dir='/database/backup/mysql/'):
         self.host=host
         self.port=port
         self.user=user
         self.password=password
+        self.backup_save_dir=backup_save_dir
 
-    def fullBackup(self):
+    def fullBackup(self,options=None):
         raise NotImplementedError("请在子类中实现对MySQL数据的全备功能.")
 
     def increaseBackup(self):
@@ -42,6 +46,9 @@ class BackupTool(object):
 
     def restoreBackup(self):
         raise NotImplementedError("请在子类中实现对备份的校验功能.")
+
+    def __repr__(self):
+        return "BackupTool(host={self.host} port={self.port} user={self.user} password={self.password} backup_script={self.backup_script})".format(self=self)
 
 class Meb(BackupTool):  
     """
@@ -59,8 +66,33 @@ class MysqlDump(BackupTool):
     """
     用mysqldump完成MySQL数据库的备份
     """
-    pass
+
+    backup_script="""mysqldump -u{self.user} -p{self.password} -h{self.host} -P{self.port} \
+    --all-databases --triggers --routines --events--set-gtid-purged=ON > {backup_save_path} &
+    """
+
+    def __repr__(self):
+        return self.backup_script.format(self=self)
+
+    def fullBackup(self,options):
+        """
+        用mysqldump完成全备
+        """
+        #创建备份的完整路径形式如
+        #/database/backup/mysql/3306/2018-4-24T21:19:44-fullbackup.sql
+        backup_save_path = os.path.join(self.backup_save_dir,
+            "{self.port}/{now.year}-{now.month}-{now.day}T{now.hour}:{now.minute}:{now.second}-fullbackup.sql".format(self=self,now=datetime.now()))
+        instance_backup_dir=os.path.dirname(backup_save_path)
+        if os.path.exists(instance_backup_dir):
+            #目录已经存在
+        else:
+            os.makedirs(instance_backup_dir)
+        os.system(self.backup_script.format(self=self,backup_save_path=backup_save_path))
+        
+
+        
 
 
 if __name__=="__main__":
-    print('hello world ...2')
+    mysqldump = MysqlDump()
+    mysqldump.fullBackup(1)
