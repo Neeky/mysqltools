52a294b595b9df6ba72e985bd885b3a4d18a9eca
Neeky <1721900707@qq.com>
2018-06-23 17:15:22 +0800

    2.18.06.22 全面兼容mysql-5.6,5.7,8.0

--- deploy/ansible/mysql/template/secure.sql
+++ deploy/ansible/mysql/template/secure.sql
@@ -1,3 +1,12 @@
+{% if 'mysql-5.6' in mysql_package %}
+set sql_log_bin=0;
+    update mysql.user set password=password('{{ mysql_root_password }}') where user='root';
+    create user {{mysql_monitor_user}}@'127.0.0.1' identified by '{{ mysql_monitor_password }}' ;
+    flush privileges; 
+set sql_log_bin=1;
+
+{% else %}
+
 set sql_log_bin=0;
     create user {{mysql_monitor_user}}@'127.0.0.1' identified by '{{ mysql_monitor_password }}' ;
     
@@ -6,4 +15,6 @@ set sql_log_bin=0;
     grant all on *.* to root@'127.0.0.1' with grant option;
 
 set sql_log_bin=1;
+
+{% endif %}
 
