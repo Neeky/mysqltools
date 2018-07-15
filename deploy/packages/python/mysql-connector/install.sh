25875838cea1aa2aa1b5e340da48d3d4fc1dec69
Neeky <1721900707@qq.com>
2018-07-15 12:44:57 +0800

    2.18.07.15 更新mysqlclient驱动程序以兼容mysql-8.0.x

--- deploy/packages/python/mysql-connector/install.sh
+++ deploy/packages/python/mysql-connector/install.sh
@@ -4,7 +4,7 @@ basedir=`dirname $0`
 echo $basedir
 cd $basedir
 
-pip3 install mysqlclient-1.3.12.tar.gz
+pip3 install mysqlclient-1.3.13.tar.gz
 pip3 install six-1.11.0-py2.py3-none-any.whl
 pip3 install protobuf-3.6.0-cp36-cp36m-manylinux1_x86_64.whl
 pip3 install mysql_connector_python-8.0.11-cp36-cp36m-manylinux1_x86_64.whl
