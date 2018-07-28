937e0b49155d101997e544eebf8cdb540fb5561b
Neeky <1721900707@qq.com>
2018-07-28 15:48:36 +0800

    2.18.07.28 在创建mysql实例时创建backup用户为后面的备份操作留下接口

--- deploy/ansible/mysql/template/secure.sql
+++ deploy/ansible/mysql/template/secure.sql
@@ -3,7 +3,26 @@ set sql_log_bin=0;
     update mysql.user set password=password('{{ mysql_root_password }}') where user='root';
     create user {{mysql_monitor_user}}@'127.0.0.1' identified by '{{ mysql_monitor_password }}' ;
     grant replication client on *.* to {{mysql_monitor_user}}@'127.0.0.1';
-    flush privileges; 
+
+    create user {{mysql_backup_user}}@'127.0.0.1' identified by '{{mysql_backup_password}}';
+    create user {{mysql_backup_user}}@'localhost' identified by '{{mysql_backup_password}}';
+    
+    grant reload,lock tables on *.* to {{mysql_backup_user}}@'127.0.0.1';
+    grant replication client on *.* to {{mysql_backup_user}}@'127.0.0.1';
+    grant create tablespace  on *.* to {{mysql_backup_user}}@'127.0.0.1';
+    grant process            on *.* to {{mysql_backup_user}}@'127.0.0.1';
+    grant super              on *.* to {{mysql_backup_user}}@'127.0.0.1';
+    grant create,insert,select      on percona_schema.xtrabackup_history to {{mysql_backup_user}}@'127.0.0.1';
+    
+    grant reload,lock tables on *.* to {{mysql_backup_user}}@'localhost';
+    grant replication client on *.* to {{mysql_backup_user}}@'localhost';
+    grant create tablespace  on *.* to {{mysql_backup_user}}@'localhost';
+    grant process            on *.* to {{mysql_backup_user}}@'localhost';
+    grant super              on *.* to {{mysql_backup_user}}@'localhost';
+    grant create,insert,select      on percona_schema.xtrabackup_history to {{mysql_backup_user}}@'localhost';
+
+    flush privileges;
+
 set sql_log_bin=1;
 
 {% else %}
@@ -15,6 +34,23 @@ set sql_log_bin=0;
     create user root@'127.0.0.1' identified by '{{ mysql_root_password }}';
     grant all on *.* to root@'127.0.0.1' with grant option;
     grant replication client on *.* to {{mysql_monitor_user}}@'127.0.0.1';
+
+    create user {{mysql_backup_user}}@'127.0.0.1' identified by '{{mysql_backup_password}}';
+    create user {{mysql_backup_user}}@'localhost' identified by '{{mysql_backup_password}}';
+    
+    grant reload,lock tables on *.* to {{mysql_backup_user}}@'127.0.0.1';
+    grant replication client on *.* to {{mysql_backup_user}}@'127.0.0.1';
+    grant create tablespace  on *.* to {{mysql_backup_user}}@'127.0.0.1';
+    grant process            on *.* to {{mysql_backup_user}}@'127.0.0.1';
+    grant super              on *.* to {{mysql_backup_user}}@'127.0.0.1';
+    grant create,insert,select      on percona_schema.xtrabackup_history to {{mysql_backup_user}}@'127.0.0.1';
+    
+    grant reload,lock tables on *.* to {{mysql_backup_user}}@'localhost';
+    grant replication client on *.* to {{mysql_backup_user}}@'localhost';
+    grant create tablespace  on *.* to {{mysql_backup_user}}@'localhost';
+    grant process            on *.* to {{mysql_backup_user}}@'localhost';
+    grant super              on *.* to {{mysql_backup_user}}@'localhost';
+    grant create,insert,select      on percona_schema.xtrabackup_history to {{mysql_backup_user}}@'localhost';
 set sql_log_bin=1;
 
 {% endif %}
