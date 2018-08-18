e75a7f9efeb9fe213d0c21ecda2e3aa03b141336
Neeky <1721900707@qq.com>
2018-08-18 16:09:33 +0800

    1.18.08.18 备份用户backup的权限明细

--- deploy/ansible/backup/template/create_backup_user.sql
+++ deploy/ansible/backup/template/create_backup_user.sql
@@ -15,4 +15,15 @@ grant process            on *.* to backup@'localhost';
 grant super              on *.* to backup@'localhost';
 grant create,insert,select      on percona_schema.xtrabackup_history to backup@'localhost';
 
+-- for mysqlbackup
+grant create,insert,drop,update               on mysql.backup_progress to backup@'127.0.0.1';
+grant create,insert,drop,update,select,alter  on mysql.backup_history  to backup@'127.0.0.1';
+
+grant create,insert,drop,update               on mysql.backup_progress to backup@'localhost';
+grant create,insert,drop,update,select,alter  on mysql.backup_history  to backup@'localhost';
+
+grant select on performance_schema.replication_group_members to backup@'127.0.0.1';
+grant select on performance_schema.replication_group_members to backup@'localhost';
+
+-- mysqltools在并没有引入mysqlbackup TTS相关功能，这一功能要求的权限通常来说是比较敏感的，而且也不常用，mysqltools在些不支持它。
 
