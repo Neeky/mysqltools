698aeb5f9cf9f0bc3ab11312922968b431b3114d
Neeky <1721900707@qq.com>
2018-08-24 10:59:16 +0800

    2.18.08.24 在批量安装python-3.6.x的情况下也自动化的安装上mysql-connector-python

--- deploy/ansible/python/template/install_python.sh
+++ deploy/ansible/python/template/install_python.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-cd /tmp/{{ mtls_python | replace('.tar.xz','') | capitalize}}
+#1、安装python
+cd /tmp/Python-3.6.2/
 ./configure --prefix=/usr/local/python-3.6.2/
 make -j 2
 make install
+
+cd /tmp/mysql-connector
+/usr/local/python-3.6.2/bin/pip3 install six-1.11.0-py2.py3-none-any.whl
+/usr/local/python-3.6.2/bin/pip3 install protobuf-3.6.0-cp36-cp36m-manylinux1_x86_64.whl
+/usr/local/python-3.6.2/bin/pip3 install mysql_connector_python-8.0.11-cp36-cp36m-manylinux1_x86_64.whl
+
+
+
