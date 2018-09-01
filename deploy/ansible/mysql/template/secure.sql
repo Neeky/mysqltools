86a2e2a3fa5a4078bf59915362726b55d057bece
Neeky <1721900707@qq.com>
2018-09-01 15:00:53 +0800

    2.18.09.01 安装mysql时自动增加monitor@'localhost' 兼容那些不开启skip-name-reslove的场景

--- deploy/ansible/mysql/template/secure.sql
+++ deploy/ansible/mysql/template/secure.sql
@@ -3,6 +3,8 @@ set sql_log_bin=0;
     update mysql.user set password=password('{{ mysql_root_password }}') where user='root';
     create user {{mysql_monitor_user}}@'127.0.0.1' identified by '{{ mysql_monitor_password }}' ;
     grant replication client on *.* to {{mysql_monitor_user}}@'127.0.0.1';
+    create user {{mysql_monitor_user}}@'localhost' identified by '{{ mysql_monitor_password }}' ;
+    grant replication client on *.* to {{mysql_monitor_user}}@'localhost';
 
     create user {{mysql_backup_user}}@'127.0.0.1' identified by '{{mysql_backup_password}}';
     create user {{mysql_backup_user}}@'localhost' identified by '{{mysql_backup_password}}';
@@ -37,12 +39,14 @@ set sql_log_bin=1;
 {% else %}
 
 set sql_log_bin=0;
-    create user {{mysql_monitor_user}}@'127.0.0.1' identified by '{{ mysql_monitor_password }}' ;
-    
     alter user root@'localhost' identified by '{{ mysql_root_password }}' ;
     create user root@'127.0.0.1' identified by '{{ mysql_root_password }}';
     grant all on *.* to root@'127.0.0.1' with grant option;
+
+    create user {{mysql_monitor_user}}@'127.0.0.1' identified by '{{ mysql_monitor_password }}' ;
     grant replication client on *.* to {{mysql_monitor_user}}@'127.0.0.1';
+    create user {{mysql_monitor_user}}@'localhost' identified by '{{ mysql_monitor_password }}' ;
+    grant replication client on *.* to {{mysql_monitor_user}}@'localhost';
 
     create user {{mysql_backup_user}}@'127.0.0.1' identified by '{{mysql_backup_password}}';
     create user {{mysql_backup_user}}@'localhost' identified by '{{mysql_backup_password}}';
